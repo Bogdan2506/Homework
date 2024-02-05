6943d8a5c60b8098bbdffb09062f51ed1a70e2af
Bogdan <kovalskiy256@gmail.com>
2024-02-05 17:08:59 +0200

    remake task2

--- task2.js
+++ task2.js
@@ -1,16 +1,23 @@
-// DZ #2(1)
+// // DZ #2(1)
 
-let sentens1 = prompt('Enter your first line here:');
-let sentens2 = prompt('Enter your second line here:');
-let sentens3 = prompt('Enter your third line here:');
+// let sentens1 = prompt('Enter your first line here:');
+// let sentens2 = prompt('Enter your second line here:');
+// let sentens3 = prompt('Enter your third line here:');
 
-let results = sentens1 + " " + sentens2 + " " + sentens3;
-console.log(results);
+// let results = sentens1 + " " + sentens2 + " " + sentens3;
+// console.log(results);
 
 // DZ #2(2)
 
-const number = 56789;
-const digits = number.toString().split("");
-let result = digits.join(" ");
 
-console.log(result);
+let number = parseInt(prompt("Введіть вашу п'ятизначне число:"));
+
+// let number = 12345; 
+
+let num5 = (number % 10) | 0;
+let num4 = ((number % 100) / 10) | 0;
+let num3 = ((number % 1000) / 100) | 0;
+let num2 = ((number % 10000) / 1000) | 0;
+let num1 = (number / 10000) | 0;
+
+console.log(num1, num2, num3, num4, num5);
