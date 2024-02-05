0a9e916ce762ca2c4c7463d7d78678b0a71a97d4
Bogdan <kovalskiy256@gmail.com>
2024-02-05 17:32:23 +0200

    task6

---
+++ task6.js
@@ -0,0 +1,58 @@
+let birthYear = prompt("Привіт. Введіть свій рік народження:");
+
+if (birthYear === null) {
+    alert("Шкода, що Ви не захотіли ввести свій рік народження.");
+} else {
+    let city = prompt("У якому місті ви живете?");
+
+
+    if (city === null) {
+        alert("Шкода, що Ви не захотіли ввести своє місто.");
+    } else {
+
+        let favoriteSport = prompt("Який ваш улюблений вид спорту?");
+
+
+        if (favoriteSport === null) {
+            alert("Шкода, що Ви не захотіли ввести свій улюблений вид спорту.");
+        } else {
+
+            let currentYear = new Date().getFullYear();
+
+
+            let age = currentYear - birthYear;
+
+
+            let message1 = "";
+            if (city.toLowerCase() === "київ") {
+                message1 = "Ти живеш у столиці України: Київ.";
+            } else if (city.toLowerCase() === "вашингтон") {
+                message1 = "Ти живеш у столиці США: Вашингтон.";
+            } else if (city.toLowerCase() === "лондон") {
+                message1 = "Ти живеш у столиці Великої Британії: Лондон.";
+            } else {
+                message1 = `Ти живеш у місті ${city}.`;
+            }
+
+
+
+            let message = `Твій вік: ${age} років.\n${message1}\n`;
+
+            let champion = "";
+            if (favoriteSport.toLowerCase() === "футбол") {
+                champion = "Кріштіану Роналду";
+            } else if (favoriteSport.toLowerCase() === "баскетбол") {
+                champion = "Леброн Джеймс";
+            } else if (favoriteSport.toLowerCase() === "теніс") {
+                champion = "Рафаель Надаль";
+            } else {
+                champion = "новим чемпіоном";
+            }
+            let message2 = `Круто! Хочеш стати ${champion}?`;
+
+
+            message += message2;
+            alert(message);
+        }
+    }
+}
